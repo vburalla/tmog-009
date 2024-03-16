e7168df91a41bdf93ea77920b13eb32311bef7d5
Victor Buralla <vburalla@mercadona.es>
2024-03-16 22:08:40 +0100

    fix: first solution

--- src/main/java/org/tfoc/Solution.java
+++ src/main/java/org/tfoc/Solution.java
@@ -1,15 +1,64 @@
 package org.tfoc;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * The class containing the solution to this exercise
  */
 public class Solution {
 
-    public Integer search(
-            Integer[] nums,
-            Integer target) {
+    public int orangesRotting(int[][] grid) {
 
-        return null;
+        List<Point> freshPoints = getFreshPoints(grid);
+
+        int i = 0;
+        if (!freshPoints.isEmpty()) {
+            Set<Point> pointsWithRottenNeighbours = getPointsWithRottenNeighbours(freshPoints, grid);
+            while (!pointsWithRottenNeighbours.isEmpty() && !freshPoints.isEmpty()) {
+                for (Point point : pointsWithRottenNeighbours) {
+                    freshPoints.remove(point);
+                    grid[point.y][point.x] = 2;
+                }
+                pointsWithRottenNeighbours = getPointsWithRottenNeighbours(freshPoints, grid);
+                i++;
+            }
+            return freshPoints.isEmpty() ? i : -1;
+        } else {
+            return 0;
+        }
+    }
+
+    private List<Point> getFreshPoints(int[][] grid) {
+
+        List<Point> freshPoints = new ArrayList<>();
+
+        for (int row = 0; row < grid.length; row++) {
+            for (int col = 0; col < grid[row].length; col++) {
+                if (grid[row][col] == 1)
+                    freshPoints.add(new Point(col, row));
+            }
+        }
+
+        return freshPoints;
+    }
+
+    private Set<Point> getPointsWithRottenNeighbours(List<Point> points, int[][] grid) {
+
+        Set<Point> withRottenNeighbours = new HashSet<>();
+
+        for (Point point : points) {
+            if ((point.x > 0 && grid[point.y][point.x - 1] == 2) ||
+                    (point.x < (grid[point.y].length - 1) && grid[point.y][point.x + 1] == 2) ||
+                    (point.y > 0 && grid[point.y - 1][point.x] == 2) ||
+                    (point.y < (grid.length - 1) && grid[point.y + 1][point.x] == 2))
+                withRottenNeighbours.add(point);
+
+        }
+        return withRottenNeighbours;
     }
 
 }
--- src/test/java/org/tfoc/SolutionTest.java
+++ src/test/java/org/tfoc/SolutionTest.java
@@ -1,15 +1,33 @@
 package org.tfoc;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Slf4j
 class SolutionTest {
 
-    @Test
-    void testSolution() {
+    @ParameterizedTest
+    @MethodSource("provideOrangeArrays")
+    void testSolutions(int[][] grid, int result) {
+        Solution solution = new Solution();
+        assertEquals(result, solution.orangesRotting(grid));
+    }
 
-        log.info("Don't forget to write the tests to make sure it works. Enjoy!");
+    private static Stream<Arguments> provideOrangeArrays() {
+        return Stream.of(
+                Arguments.of(new int[][]{{2,1,1},{1,1,0},{0,1,1}}, 4),
+                Arguments.of(new int[][]{ { 2, 1, 1 },{ 0, 1, 1 },{ 1, 0, 1 } }, -1),
+                Arguments.of(new int[][]{ { 0, 2 }}, 0),
+                Arguments.of(new int[][]{{2,0,1,1,1,1,1,1,1,1},{1,0,1,0,0,0,0,0,0,1},{1,0,1,0,1,1,1,1,0,1},{1,0,1,0,1,0,0,1,0,1},{1,0,1,0,1,0,0,1,0,1},{1,0,1,0,1,1,0,1,0,1},{1,0,1,0,0,0,0,1,0,1},{1,0,1,1,1,1,1,1,0,1},{1,0,0,0,0,0,0,0,0,1},{1,1,1,1,1,1,1,1,1,1}}, 58)
+        );
     }
 
 }
